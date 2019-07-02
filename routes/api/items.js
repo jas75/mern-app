5258f63ff74ec6f0d65d6fa11783dbb4ba5e620d
Jason <azoulayjason@gmail.com>
2019-07-02 03:49:29 +0200

    Correction

--- routes/api/items.js
+++ routes/api/items.js
@@ -30,7 +30,9 @@ router.post('/', (req, res) => {
 // @access  Public
 router.delete('/:id', (req, res) => {
     Item.findById(req.params.id)
-    .then(item => item.remove().then(res.json({ success: true })))
-    .catch(err => res.status(404).json({ success: false, msg: err}));
+    .then(item => item.remove().then(res.json({ success: true})))
+    .catch(err => res.status(404).json({ success: false}));
 });
+
+
 module.exports = router;
