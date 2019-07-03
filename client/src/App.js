fdb70216dd693ecade182f8d11bbdd04a2ccabde
Jason <azoulayjason@gmail.com>
2019-07-04 00:39:47 +0200

    create reducers

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,21 @@ import ShoppingList from './components/ShoppingList';
 
 import { Provider } from 'react-redux';
 import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      
-        <AppNavbar />
+      <Provider store={store}>
+        <div className="App">
+        
+          <AppNavbar />
 
-        <ShoppingList />
-      </div>
+          <ShoppingList />
+        </div>
+      </Provider>
     );
   }
 }
