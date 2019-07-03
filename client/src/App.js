cf34bc8fc6f0617873734886def98990ffc3cfba
Jason <azoulayjason@gmail.com>
2019-07-04 00:22:32 +0200

    create redux store

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShoppingList from './components/ShoppingList';
 
+import { Provider } from 'react-redux';
+import store from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
